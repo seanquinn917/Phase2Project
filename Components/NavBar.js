93648e4c3a1f490118b11a45a53c5b3e3ea13aaf
SeanQuinn <Seanquinn917@gmail.com>
2023-08-21 21:45:58 -0400

    centered navBar CSS

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom"
 function NavBar(){
     return(
         <div className=".topnav">
+            
             <h1>
-            <span>
             <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/Buglist">Bugs!</Link></li>
             <li><Link to="/SubmitBug">Add your favorite Bug!</Link></li>
             </ul>   
-            </span>    
             </h1>
+            
         </div>
     )
 }
