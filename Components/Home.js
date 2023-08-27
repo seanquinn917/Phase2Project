3754faed1041dd322cf6400ab3c09746c290da82
SeanQuinn <Seanquinn917@gmail.com>
2023-08-27 17:46:33 -0400

    update text within Home Page

--- Components/Home.js
+++ Components/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 
+
 function Home(){
 
 
 
     return (
         <div>
-            <span>
-                <h4>Home</h4>
+            <span >
+                <h4>Welcome to the world of Pollinators</h4>
+                <p className="welcome">As a pollinator garden enthusiast, I have a strong curiousity for what is flying about.  Here's a small glimpse into my favorite flying friends!</p>
                 <img src= "https://images.cnbctv18.com/wp-content/uploads/2023/04/Earth-Day-1019x573.jpg?impolicy=website&width=617&height=264" alt="hmmm"></img>
             </span>
         </div>
